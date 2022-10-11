3eeeaa55c121cdc3ea1a5917e49b907624b804b9
laynejohnson13 <laynejohnson13001@gmail.com>
2022-10-11 12:09:11 -0400

    surf surf_shop_scraped added, flyingpoint.py edited

---
+++ flyingpoint.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+
+####upload Flying Point Surf Shop data
+
+surf_shop = requests.get('https://www.flyingpointsurf.com/womens/dresses/')
+
+##make beautifulsoup object
+soup = BeautifulSoup(surf_shop.text, 'html.parser')
+
+print(soup.prettify())
+
+
+##Pull clothing items listed
+item = soup.find_all('h1', class_ = "text-base margin-bottom-xs")
+output_item = []
+for i in item:
+    print(i.text)
+    data = i.text
+    item.append(data)
+
+len(output_item)
+output_item[0]
+output_item[3]
+
+
+
+
+
+##Pull prices for items
+price = soup.find_all('ins', class_ = "prod-card__price")
+output_price = []
+for i in price:
+    print(i.text)
+    data = i.text
+    price.append(data)
+
+len(output_price)
+output_price[0]
+output_price[3]
+
+
+list1 = item 
+list2 = price
+
+
+# create dictionary
+dictionary = {'item': list1, 'price': list2}
+
+## convert to dataframe
+df = pd.DataFrame({'item':item,'price':price})
+
+##convert to csv 
+df.to_csv('/Users/laynejohnson/Desktop/HHA507:504 - classwork/web-scraping/data/surf_shop_scraped')
